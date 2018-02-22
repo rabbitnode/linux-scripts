a6358da07e16d45c3e50d8b443c1195333353357
asapcatftw <aasyal@my.harrisburgu.edu>
2018-02-21 23:48:52 -0500

    i cant even right now

--- Flarum/install.sh
+++ Flarum/install.sh
@@ -34,12 +34,13 @@ firewall-cmd --permanent --add-port=22/tcp
 firewall-cmd --permanent --add-port=21/tcp
 firewall-cmd --reload
 #
-
-echo " | "
-echo " / "
-echo " _ "
-echo " \ "
-echo " | "
+yum -y install yum-utils
+yum -y update
+yum -y install php
+yum-config-manager --enable remi-php71
+systemctl restart httpd.service
+#
+yum -y install php-mcrypt php-cli php-gd php-curl php-mysql php-dom php-ldap php-zip php-fileinfo php-mbstring mysql-server -y
 #
 sudo systemctl start mysqld
 sudo systemctl enable mysqld
@@ -50,15 +51,6 @@ mysql -e "DROP USER ''@'$(hostname)'"
 mysql -e "DROP DATABASE test"
 mysql -e "FLUSH PRIVILEGES"
 #
-yum -y install yum-utils
-yum -y update
-yum -y install php
-yum-config-manager --enable remi-php71
-systemctl restart httpd.service
-#
-yum -y install php-mcrypt php-cli php-gd php-curl php-mysql php-dom php-ldap php-zip php-fileinfo php-mbstring mysql-server -y
-#
-
 echo ""
 echo "#################################################"
 echo "     Visit your domain to finish the install     "
