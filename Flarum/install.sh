e8be1ed153f22bbc02ee5a35256a935dc19f5455
asapcatftw <aasyal@my.harrisburgu.edu>
2018-02-21 23:09:59 -0500

    Updated: MySQL section start/enable firewalld
    Added:
    php

--- Flarum/install.sh
+++ Flarum/install.sh
@@ -20,6 +20,8 @@ yum install nano zip unzip wget curl httpd firewalld -y
 systemctl start httpd.service
 systemctl enable httpd.service
 #
+systemctl start firewalld
+systemcrl enable firewalld
 firewall-cmd --permanent --zone=public --add-service=http
 firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --permanent --add-port=22/tcp
@@ -30,10 +32,29 @@ yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.r
 yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y
 wget http://repo.mysql.com/mysql-community-release-el7-5.noarch.rpm
 rpm -ivh mysql-community-release-el7-5.noarch.rpm
+systemctl start mysqld.service
+systemctl enable mysqld.service
 #
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$mysql_password') WHERE User = 'root'"
+mysql -e "DROP USER ''@'localhost'"
+mysql -e "DROP USER ''@'$(hostname)'"
+mysql -e "DROP DATABASE test"
+mysql -e "FLUSH PRIVILEGES"
+#
+yum -y install yum-utils
+yum -y update
+yum -y install php
+yum-config-manager --enable remi-php71
+systemctl restart httpd.service
+#
+yum -y install php-mcrypt php-cli php-gd php-curl php-mysql php-dom php-ldap php-zip php-fileinfo php-mbstring mysql-server -y
+#
+
 echo ""
-echo "################################################"
-echo "#          Flarum has been installed.          #"
-echo "################################################"
+echo "#################################################"
+echo "     Visit your domain to finish the install     "
+echo "     MySQL rootuser: root                        "
+echo "     MySQL password: $mysql_password             "
+echo "#################################################"
 #
 exit
